40b002ca2a5a4e6cdb6a79ae470b502d0b26267d
jaianea <147431399+jaianea@users.noreply.github.com>
2023-11-08 11:48:53 -0300

    Add files via upload
    atividade 07

---
+++ atividade07.py
@@ -0,0 +1,10 @@
+real = float(input("Digite o Valor em Real"))
+dolar = float(input("Digite a cotação do dia!"))
+
+conv = real / dolar
+
+print(f"""
+    Seu Valor em R$ {real}
+    Cotação atual em $ {dolar}
+    Valor de Convesão $ {conv}
+""")
